d6392d9546255d6296d8cbeb59fccd399ea1ff82
Charishma Priya <charishmapriya.5@gmail.com>
2025-07-08 01:52:14 +0530

    Update 1.py

--- Basics/1.py
+++ Basics/1.py
@@ -1,1 +1,4 @@
-print("Hello world") #
+print() #output : prints a blank space
+print("Hello world") #output : Hello World
+print('Hey') #output : Hey
+print("'How are you?'") #output : 'How are you?'
